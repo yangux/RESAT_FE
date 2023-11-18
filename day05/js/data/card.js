ef0392ee1e9da00d8db978dd0a86ef7a75e2f277
yangux <yeonie05@gmail.com>
2023-11-18 21:07:56 +0900

    fix(day05) - 이미지 경로 수정

--- day05/js/data/card.js
+++ day05/js/data/card.js
@@ -5,7 +5,7 @@ const intern = [
     job: "기획·비즈니스",
     title:
       "글로벌 시대에 맞춰 전세계 누구나 쉽게 즐길 수 있는 영상(숏폼) 콘텐츠 기획",
-    img: "/day05/img/card_intern_1.png",
+    img: "./img/card_intern_1.png",
   },
   {
     category: "교육형",
@@ -13,42 +13,42 @@ const intern = [
     job: "마케팅",
     title:
       "펀딩 프로젝트 성공을 위한 구름맨션 브랜드 인지도 향상 SNS 마케팅 콘텐츠 기획",
-    img: "/day05/img/card_intern_2.png",
+    img: "./img/card_intern_2.png",
   },
   {
     category: "채용형",
     dDay: "D-8",
     job: "마케팅",
     title: "빈크런치 브랜드 및 제품을 효과적으로 알리기 위한 SNS 콘텐츠 제작",
-    img: "/day05/img/card_intern_3.png",
+    img: "./img/card_intern_3.png",
   },
   {
     category: "교육형",
     dDay: "D-2",
     job: "마케팅",
     title: "유튜브 쇼핑 채널을 효과적으로 홍보할 수 있는 마케팅 방안 제안",
-    img: "/day05/img/card_intern_4.png",
+    img: "./img/card_intern_4.png",
   },
   {
     category: "교육형",
     dDay: "D-10",
     job: "디자인",
     title: "주얼리 커머스 플랫폼 및 브랜드 디자인 컨셉안 기획",
-    img: "/day05/img/card_intern_5.png",
+    img: "./img/card_intern_5.png",
   },
   {
     category: "채용형",
     dDay: "D-5",
     job: "기획·비즈니스",
     title: "기능성 식품 브랜드 '레디블룸' 신규 타겟 확장 방안 기획",
-    img: "/day05/img/card_intern_6.png",
+    img: "./img/card_intern_6.png",
   },
   {
     category: "채용형",
     dDay: "D-5",
     job: "기획·비즈니스",
     title: "2030세대가 투표를 기반으로 소통할 수 있는 커뮤니티 기획 및 구축",
-    img: "/day05/img/card_intern_7.png",
+    img: "./img/card_intern_7.png",
   },
   {
     category: "교육형",
@@ -56,7 +56,7 @@ const intern = [
     job: "디자인",
     title:
       "펀딩 프로젝트 성공을 위한 구름맨션 브랜드 인지도 향상 SNS 마케팅 콘텐츠 기획",
-    img: "/day05/img/card_intern_8.png",
+    img: "./img/card_intern_8.png",
   },
 ];
 
