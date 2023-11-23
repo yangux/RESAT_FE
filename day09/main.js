1ed0985611a0cce45092e67e60cbc1c7012eae92
yangux <yeonie05@gmail.com>
2023-11-23 23:12:10 +0900

    day09 - 닫기 버튼, 배경 dim 추가

--- day09/main.js
+++ day09/main.js
@@ -1,10 +1,16 @@
 const body = document.querySelector("body");
+const modal = document.querySelector(".modal");
 const menuBtn = document.querySelector(".gnb .menuBtn");
 const navMenu = document.querySelector(".gnb ul");
+const exitBarFirst = menuBtn.querySelector("span:first-child");
+const exitBarMiddle = menuBtn.querySelector("span:nth-child(2)");
+const exitBarLast = menuBtn.querySelector("span:last-child");
 
 menuBtn.addEventListener("click", function () {
+  transformExitBtn();
   navMenu.classList.add("animate");
   navMenu.classList.toggle("mobile");
+  modal.classList.toggle("visible");
 
   if (navMenu.classList.contains("mobile")) {
     // open
@@ -17,3 +23,9 @@ menuBtn.addEventListener("click", function () {
     navMenu.classList.remove("animate");
   }, 1510);
 });
+
+function transformExitBtn() {
+  exitBarMiddle.classList.toggle("hide");
+  exitBarFirst.classList.toggle("exitBarFirst");
+  exitBarLast.classList.toggle("exitBarLast");
+}
